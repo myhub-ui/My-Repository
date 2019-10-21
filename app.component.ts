9d7cec5186b4f6714c20dd8126ac1ea3388ea731
Swati <swati232391@gmail.com>
2019-10-21 22:47:18 +0200

    Initial commit

---
+++ app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numbers=[];
+
+  constructor(){
+    for (let i=0;i<10000;i++) {
+      this.numbers.push(i)
+    }
+  }
+}
---
+++ app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{ ScrollingModule} from'@angular/cdk/scrolling';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ScrollingModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
